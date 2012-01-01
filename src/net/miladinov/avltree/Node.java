36ff2097741d88683c48e93062b044874bd6cac8
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-01-01 11:27:04 -0500

    Added a reference to a Node's parent Node.
    . . . and it simplified a lot of the remove / unlink logic. It also exposed the need to implement Node.rightMost(), where it was more at home, rather than embedded inside of AvlTree.unlink().

--- src/net/miladinov/avltree/Node.java
+++ src/net/miladinov/avltree/Node.java
@@ -1,18 +1,24 @@
 package net.miladinov.avltree;
 
 class Node<T extends Comparable<? super T>> {
+    private Node<T> parent;
     private Node<T> right;
     private Node<T> left;
     private T data;
 
     public Node(T data) {
-        this(data, null, null);
+        this(data, null, null, null);
     }
 
-    public Node(T data, Node<T> left, Node<T> right) {
+    public Node(T data, Node<T> parent, Node<T> left, Node<T> right) {
         this.data = data;
+        this.parent = parent;
         this.left = left;
         this.right = right;
+    }
+
+    public Node<T> parent() {
+        return parent;
     }
 
     public Node<T> left() {
@@ -21,6 +27,14 @@ class Node<T extends Comparable<? super T>> {
 
     public Node<T> right() {
         return right;
+    }
+
+    public Node<T> rightMost() {
+        if (right != null) {
+            return right.rightMost();
+        } else {
+            return this;
+        }
     }
 
     public T data() {
